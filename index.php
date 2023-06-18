81942d930ca1859682c96dfb86fe1f72c4c4313a
goayasushi <goayasushi@gmail.com>
2023-06-18 09:37:04 +0900

    20230618

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>デイトラ</title>
-</head>
-<body>
-  <h1>こんにちは</h1>
-</body>
-</html>
+<h1>
+  <?php
+    $score = 80;
+    if ($score > 79) {
+      echo 'すごい！よく頑張ったね！';
+    }
+  ?>
+</h1>
