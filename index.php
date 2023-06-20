3dc7a7038caa53cc0396e01035bef6f361dffffd
goayasushi <goayasushi@gmail.com>
2023-06-20 11:31:48 +0900

    20230620

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
-<?php
-  $scores = [
-    "数学" => 89,
-    "英語" => 76,
-    "理科" => 35,
-    "社会" => 83
-  ];
-  echo $scores["理科"];
-?>
+<?php require_once 'products.php'; ?>
+<?php require_once 'functions.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./style.css" >
+  <title>デイトラ PHP</title>
+</head>
+<body>
+  <div class="container">
+    <div class="app-container">
+      <h1 class="title">DailyTrial Shopping</h1>
+      <form id="cart" method="post" action="cart.php">
+        <div class="cards-container">
+          <?php foreach($products as $product):?>
+            <div class="card">
+              <img class="card-image" src="<?php echo $product["image"]; ?> " alt="">
+              <p class="card-title"><?php echo $product["name"]; ?></p>
+              <div class="flex justify-between">
+                <p class="card-price"><?php echo displayPrice($product["price"]); ?></p>
+                <input name="<?php echo $product["id"]; ?>" min="0" class="item-number" type="number" value="0">
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </form>
+      <div class="btn-footer bg-white">
+        <input form="cart" class="cart-btn" type="submit" name="submit" value="カートに追加" />
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,29 @@
+<?php
+  $products = [
+    [
+      "id" => "avocado",
+      "name" => "アボカド",
+      "price" => 230,
+      "image" => "./images/image_1.jpg"
+    ],
+    [
+      "id" => "orange",
+      "name" => "オレンジ",
+      "price" => 100,
+      "image" => "./images/image_2.jpg"
+    ],
+    [
+      "id" => "lemon",
+      "name" => "レモン",
+      "price" => 200,
+      "image" => "./images/image_3.jpg"
+    ],
+    [
+      "id" => "pine",
+      "name" => "パイン",
+      "price" => 400,
+      "image" => "./images/image_4.jpg"
+    ]
+  ];
+?> 
+
