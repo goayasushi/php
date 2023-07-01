c0aeaa11aeb9cdc64ae6d7ebfe0746494501d8ec
goayasushi <goayasushi@gmail.com>
2023-07-01 14:53:17 +0900

    20230701

--- dev/wp-content/themes/tf30/functions.php
+++ dev/wp-content/themes/tf30/functions.php
@@ -13,6 +13,9 @@
     wp_enqueue_style("my", get_template_directory_uri() . "/css/style.css", array(), filemtime(get_theme_file_path('css/style.css')), "all");
     wp_enqueue_script("my", get_template_directory_uri() . "/js/script.js", array("jquery"), filemtime(get_theme_file_path('js/script.js')), true);
 
+    if (is_single()) {
+      wp_enqueue_script("sns", get_template_directory_uri() . "/js/sns.js", array("jquery"), filemtime(get_theme_file_path('js/sns.js')), true);
+    }
   }
   add_action("wp_enqueue_scripts", "my_script_init");
 
